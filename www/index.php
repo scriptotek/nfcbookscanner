0bef72ebe5b843bc78b9ec9a6b645bff08da836d
Dan Michael O. Heggø <danmichaelo@gmail.com>
2013-06-21 13:33:44 +0200

    add web interface

---
+++ www/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta charset="utf-8" />
+    <title>Grunnleggende bokinfo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="jquery-1.9.1.min.js"></script>
+    <!--<script src="bootstrap/js/bootstrap.min.js"></script>-->
+    <script type="text/javascript" src="main.js"></script> 
+
+	<link rel="stylesheet" type="text/css" href="holo-web/holo-base-elements.css" />
+	<link rel="stylesheet" type="text/css" href="holo-web/holo-light-elements.css" />
+        <!--
+
+<link rel="stylesheet" type="text/css" href="holo-web/holo-base-widgets.css" />
+		<link rel="stylesheet" type="text/css" href="holo-web/holo-dark-widgets.css" />
+-->		
+		<script type="text/javascript" src="holo-web/holo-touch.js"></script>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1.2, minimum-scale=1.2, maximum-scale=1.2, user-scalable=yes" />
+
+<!--
+<script src="jquery.mobile-1.3.1.min.js"></script>
+<link rel="stylesheet" href="web-holo/AndroidHoloDarkLight.min.css" />
+<link rel="stylesheet" href="jquery.mobile.structure-1.3.1.min.css" />
+<link rel="stylesheet" href="holo.css" />
+<link rel="stylesheet" href="roboto/fonts.css" />
+-->
+
+<!--
+    <link href='http://fonts.googleapis.com/css?family=Aldrich' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+    
+    <link href='http://fonts.googleapis.com/css?family=Quantico' rel='stylesheet' type='text/css'>
+
+    <style>
+html,  body{
+background: #111111;
+color: #ffffff;
+font-family: roboto;
+}
+    	div {
+    		font-size: 1.1em;
+    		line-height: 100%;
+    	}
+    </style>
+-->
+</head>
+<body>
+
+	<div id="msg"></div>
+
+    <div style="border-top: 1px solid #aaa; margin-top: 20px; padding-top: 10px;"/>
+    <div class="holo-buttons">
+        <button class="holo-button">Lån</button>
+        <button class="holo-button">Husk</button>
+        <button class="holo-button">Stjel</button>
+    </div>
+
+</body> 
+</html>
